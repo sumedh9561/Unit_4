de8e377631ee2a1528d76d6ef7476b4e84a836db
Sumedh Ambhore <103955283+sumedh9561@users.noreply.github.com>
2022-10-15 00:38:58 +0530

    Add files via upload
    a

---
+++ Day__7/src/problem_2/Demo.java
@@ -0,0 +1,19 @@
+package problem_2;
+
+import java.util.Scanner;
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
+        String str=sc.nextLine();
+        char[] chr=str.toCharArray();
+
+        char charTwo=chr[2];
+        System.out.println(str);
+        System.out.println(charTwo);
+    }
+}
+
+
