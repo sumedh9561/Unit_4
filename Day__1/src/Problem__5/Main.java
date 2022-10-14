96aab0eeb3632018008e75543283c81bda20a635
Sumedh Ambhore <103955283+sumedh9561@users.noreply.github.com>
2022-10-15 00:33:33 +0530

    Add files via upload
    a

---
+++ Day__1/src/Problem__5/Main.java
@@ -0,0 +1,36 @@
+package Problem__5;
+
+public class Main {
+
+    public static void main(String[]args) {
+
+        String Name="XYZ";
+        String Fathers_Name="XYZ";
+        String Mother_Name="XYZ";
+        int Age =23;
+        String Gender="Male";
+        String Addres="At.post-XYZ,Ta-XYZ,Dist-XYZ Pincode-444001";
+        long Mobile_No=1234567890;
+
+        System.out.println("My name is " + Name);
+        System.out.println("My Father name is " + Fathers_Name);
+        System.out.println("My Mother name is " + Mother_Name);
+        System.out.println("Age is " + Age +"yr");
+        System.out.println("Gender :- " + Gender);
+        System.out.println("Address :- " + Addres);
+        System.out.println("Mobile No :- " + Mobile_No);
+
+
+    }
+
+
+}
+
+
+//        Example OutputName : XYZ
+//        Father’s Name : XYZ
+//        Mother‘s Name : XYZ
+//        Age : XYZ
+//        Gender : XYZ
+//        Address : XYZ
+//        Mobile No. : XYZ
